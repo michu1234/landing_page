279293844cc3f8b66b46ea7c923178d9c0174865
unknown <dawiddeveloper@gmail.com>
2018-12-15 04:10:20 +0100

    responsive bugx fixed

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,2 +1,37 @@
 import {TweenLite} from "gsap/TweenMax";
-import '../scss/main.scss'
+import '../scss/main.scss';
+
+
+
+let scrollButton = document.querySelectorAll(".btn__scroll");
+let popupButton = document.querySelectorAll(".btn__popup");
+let footer = document.querySelector("footer");
+let flooring = document.querySelector(".flooring");
+let popup = document.querySelector(".popup");
+let popupClose = document.querySelector(".popup__close");
+
+let scrollToElements = [footer, flooring];
+
+// scrolling to
+
+scrollButton.forEach((el,i)=> {
+    el.addEventListener('click', ()=> {
+        scrollToElements[i].scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+    })
+})
+
+
+//   popup on/off
+
+popupButton.forEach((el,i)=> {
+    el.addEventListener('click', ()=> {
+            popup.classList.add("popup--visible");
+    })
+})
+
+
+popupClose.addEventListener('click', ()=> {
+            popup.classList.remove("popup--visible");
+    })
+
+
